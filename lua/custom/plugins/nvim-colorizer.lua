b9a4234d2d10ea85c5f07134def690adbd8ecd12
leftZ <trilaf12345@gmail.com>
2026-05-09 18:36:17 -0400

    Added standalone files for custom plugins

---
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  'catgoose/nvim-colorizer.lua',
+  event = 'BufReadPre',
+  opts = {
+    rgb_fn = true,
+    css = true,
+    css_fn = true,
+    xterm = true,
+  },
+}
---
+++ lua/custom/plugins/rustaceanvim.lua
@@ -0,0 +1,46 @@
+return {
+  'mrcjkb/rustaceanvim',
+  lazy = false,
+  -- ft = { 'rust' },
+  init = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set('n', '<leader>cR', function()
+            vim.cmd.RustLsp 'codeAction'
+          end, { desc = 'Code Action', buffer = bufnr })
+          vim.keymap.set('n', '<leader>dr', function()
+            vim.cmd.RustLsp 'debuggables'
+          end, { desc = 'Rust Debuggables', buffer = bufnr })
+        end,
+        default_settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = { enable = true },
+            },
+            checkOnSave = true,
+            diagnostics = { enable = true },
+            procMacro = {
+              enable = true,
+            },
+            files = {
+              excludeDirs = {
+                '.direnv',
+                '.git',
+                '.github',
+                '.gitlab',
+                'bin',
+                'node_modules',
+                'target',
+                'venv',
+                '.venv',
+              },
+            },
+          },
+        },
+      },
+    }
+  end,
+}
